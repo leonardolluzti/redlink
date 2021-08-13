5c948576a413b9b32aed639cda7d2c7c49e18509
Leonardo Lopes da Luz <leonardolluzti@gmail.com>
2021-08-13 17:24:12 -0300

    Projeto Iniciado

---
+++ src/main/java/redlink/RedlinkApplication.java
@@ -0,0 +1,13 @@
+package redlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/redlink/RedlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package redlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
